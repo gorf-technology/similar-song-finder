8a5e6d9b55a21e163802c10b064f1eba3b294640
gorf-technology <limbyeongchang110@gmail.com>
2026-07-21 23:51:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// 캐시 버전 - index.html의 version-tag 표시(v1.0)와 항상 같이 올릴 것
+const CACHE_NAME = 'similar-song-finder-v1.0';
+
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // last.fm / iTunes 같은 외부 API 호출은 캐시하지 않고 그대로 네트워크로
+  if (!event.request.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
